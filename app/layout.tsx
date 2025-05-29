ed1cd689d991b924ccefe6e8acd3dcc78f6f0c5b
tomguillermou <tomguillermou@gmail.com>
2025-05-29 18:27:23 +0200

    Fix spacing on mobile

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
   return (
     <html lang="fr-FR">
       <body>
-        <div className="mx-auto max-w-lg py-8">
+        <div className="mx-auto max-w-lg p-8 md:px-0">
           <h1 className="mb-8 text-center text-2xl font-bold">Mon Petit Cochon</h1>
 
           {children}
